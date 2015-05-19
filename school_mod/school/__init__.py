323107e8b15dbc6685db4a886d77a3e88cb28656
Ashraf Fouda <afouda@jodod.com>
2015-05-19 18:30:14 +0200

    Adds school module
    Signed-off-by: Ashraf Fouda <afouda@jodod.com>

---
+++ school_mod/school/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+import controllers
+import models
+from models import *
---
+++ school_mod/school/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+class school(models.Model):
+    _name = 'school.school'
+    name = fields.Char(required=True)
+
+class Student(models.Model):
+    _name = 'school.student'
+    name = fields.Char(required=True)
+    age = fields.Integer (required=True)
+    gender = fields.Selection([
+         ('male', "Male"),
+         ('female', 'Female'),
+    ], default="male",
+    required=True)
+    photo = fields.Binary("Image")
+    student_class = fields.Many2one("school.class",  ondelete='cascade', string="Class", required=True)
+    user = fields.Many2one("res.users",  ondelete='cascade', string="User", required=True)
+
+class Course(models.Model):
+    _name = 'school.course'
+    name = fields.Char(required=True)
+    starts_at = fields.Date(required=True)
+    ends_at = fields.Date(required=True)
+    book = fields.Binary(required=True)
+    photo = fields.Binary("Image", required=True)
+    course_class = fields.Many2one("school.class",  ondelete='cascade', string="Class", required=True)
+
+
+class Class(models.Model):
+    _name = 'school.class'
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    end_date = fields.Date()
+    duration = fields.Integer()
+    grades = fields.Float(required=True)
+    courses = fields.Many2many("school.course",  ondelete='cascade', string="Course")
+    students = fields.Many2many("school.student",  ondelete='cascade', string="Students")
+
+
+
+
